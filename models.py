a17309454ed947e7ad4c800e95b69fb543b53357
Edgard Pimentel <pimentelrojas@gmail.com>
2016-03-28 22:39:07 -0500

    creacion de nuevo metodo para la firma

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 # coding: utf-8
-from jinja2 import Template, FileSystemLoader, Environment
 
+
+from jinja2 import Template, FileSystemLoader, Environment
+from signxml import xmldsig, methods
+
+import os, sys
+import zipfile
+from lxml import etree
+
+
+path_dir = os.path.dirname(os.path.realpath(__file__))
+attach_dir = os.path.join(path_dir, 'attach')
 loader = FileSystemLoader('./templates')
 env = Environment(loader=loader)
 
@@ -9,9 +19,10 @@ class Document(object):
 
     template_name = ''
 
-    def __init__(self, data):
+    def __init__(self, data, filename):
         self._data = data
         self._xml = None
+        self.filename = filename
 
     def validate(self):
         """
@@ -28,6 +39,31 @@ class Document(object):
         implement signature process
         """
 
+    # def out_xml(self):
+    #    file_xml = os.path.join(attach_dir, self.filename)
+    #    with open(file_xml, 'w') as f:
+    #        f.write(self._xml)
+
+    def out_zip(self):
+        cert = open("cert.pem").read()
+        key = open("key.pem").read()
+        zf = zipfile.ZipFile('Invoice.zip', mode='w', compression=zipfile.ZIP_DEFLATED)
+        file_xml = os.path.join(attach_dir, self.filename)
+        root = etree.fromstring(self._xml.encode('ISO-8859-1'), parser=etree.XMLParser(encoding='ISO-8859-1'))
+        signed_root = xmldsig(root, digest_algorithm='sha1').sign(method= methods.enveloped,
+                                                                  algorithm='rsa-sha1',
+                                                                  passphrase=None,
+                                                                  key=key, cert=cert)
+        self._xml = etree.tostring(root)
+        #signed_root = etree.tostring()
+        #verified_data = xmldsig(signed_root).verify()
+
+        print self._xml
+        zf.writestr(self.filename, self._xml)
+        #file_xml = os.path.join(attach_dir, self.filename)
+        #zf.write(os.walk(file_xml))
+        zf.close()
+
     def process(self):
         """
         implement soap connection here
@@ -38,8 +74,10 @@ class Document(object):
         self.validate()
         self.sign()
         self.render()
+        #self.out_xml()
+        self.out_zip()
         self.process()
-        return self._xml
+        # return self._xml
 
 
 class Invoice(Document):
@@ -48,3 +86,4 @@ class Invoice(Document):
 
     def validate(self):
         pass
+
